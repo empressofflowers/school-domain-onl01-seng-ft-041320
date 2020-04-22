7cb4add3a153d42768227ea5d5834e5a6ce23cc2
Devone Austin <devone.austin@gmail.com>
2020-04-22 18:22:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,7 +14,9 @@ class School
   end
 
   def add_student(student_name, grade)
-
+    if !@roster.include?(grade)
+      @roster[grade] = []
+      @roster[grade] << "#{student_name}"
     #student.each_with_index { |student_name, grade| roster << "#{student_name}, #{grade}" }
   end
 
