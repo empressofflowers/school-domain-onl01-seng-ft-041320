6a783e92d36ef437b00f979d5877c7563a9ccebc
Devone Austin <devone.austin@gmail.com>
2020-04-22 19:09:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,25 @@
 # code here!
 
 class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = []
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(grade, student_name)
+    roster.each {|grade, student_name| roster[grade] => "#{student_name}" }
+  end
+
+  def sort
     roster_sorted = []
-    roster.each {|grade, students| roster[grade] = students.sort}
+    roster.each {|grade, students| roster_sorted[grade] = students.sort}
     roster_sorted
+  end
+
 end
