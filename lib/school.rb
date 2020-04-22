80cece475761cc5665bfedd2694a2a41eaf772bb
Devone Austin <devone.austin@gmail.com>
2020-04-22 17:52:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 # code here!
 
 class School
-  attr_reader :name
+  attr_reader :name, :roster
 
   def initialize(name)
     @name = name
@@ -13,14 +13,14 @@ class School
   end
 
   def add_student(student)
-    student.each_with_index { |student_name, grade|  }
+    student.each_with_index { |student_name, grade| roster << "#{student_name}, #{grade}" }
   end
 
-  def grade
+  def grade(student)
 
   end
 
   def sort
-
+    add_student.sort
   end
 end
